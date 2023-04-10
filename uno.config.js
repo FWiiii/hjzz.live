87192f0cc71e493c048bad53f037caedc64bd804
FW <843189132@qq.com>
2023-04-10 17:14:57 +0800

    refactor:Use Layout for layout

--- uno.config.js
+++ uno.config.js
@@ -4,21 +4,38 @@ import {
   presetIcons,
   presetTypography,
   presetUno,
+  presetWebFonts
 } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
+    presetWebFonts({
+      provider: 'none',
+      fonts: {
+        pt: 'pt-sans'
+      }
+    }),
     presetIcons({
       scale: 1.1,
       cdn: 'https://esm.sh/',
     }),
     presetTypography({
       cssExtend: {
-        'ul,ol': {
-          'padding-left': '2.25em',
-          'position': 'relative',
+        'h1': {
+          'font-size': '1.6em',
+        },
+        'h2': {
+          'font-size': '1.4em',
+          'margin': '1.4em 0 0.5em',
+        },
+        'h3': {
+          'font-size': '1.25em',
+          'margin': '1.2em 0 0.5em',
+        },
+        'h5, h6': {
+          margin: '1em 0 0.5em',
         },
       },
     }),
