51d6de58cf7965e8dc10de00233eac8cbe51821c
FW <843189132@qq.com>
2023-04-10 16:38:35 +0800

    chore:use unocss

---
+++ uno.config.js
@@ -0,0 +1,27 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+} from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      scale: 1.1,
+      cdn: 'https://esm.sh/',
+    }),
+    presetTypography({
+      cssExtend: {
+        'ul,ol': {
+          'padding-left': '2.25em',
+          'position': 'relative',
+        },
+      },
+    }),
+  ],
+  shortcuts: [],
+})
